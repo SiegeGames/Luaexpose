ae48babd55c23f886ef9f3a81c87e62208b65b4b
Andrew Spiering <aspiering@gmail.com>
2020-12-24 01:33:14 -0800

    Fixing some issues with CI

--- Extras/build_all.sh
+++ Extras/build_all.sh
@@ -6,3 +6,5 @@ echo "Building OSX-x64"
 dotnet publish LuaExpose.csproj -r osx-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
 echo "Building Linux-x64"
 dotnet publish LuaExpose.csproj -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
+echo "Building ubuntu.18.04-x64"
+dotnet publish LuaExpose.csproj -r ubuntu.18.04-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
